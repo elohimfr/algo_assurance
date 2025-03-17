829f14c03824c6720266a10e8fbf300e35de2b69
Elohim Fonseca dos Reis <elohimfr@gmail.com>
2025-03-17 12:13:12 +0000

    Updating files.

--- fraudmrm/mlp.py
+++ fraudmrm/mlp.py
@@ -2,75 +2,84 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import pandas as pd
 from torch.utils.data import DataLoader
-from torchmetrics.classification import BinaryRecall, BinaryPrecision, BinaryF1Score, BinaryAccuracy
+from torcheval.metrics.classification import BinaryRecall, BinaryPrecision, BinaryF1Score, BinaryAccuracy
+from fraudmrm.utils import TrainData, TestData
 
 
-class BinaryMLP(nn.Module):
+class BinaryMLP1L(nn.Module):
     def __init__(self, 
                  input_size: int,
-                 layer1_size: int=None,
-                 layer2_size: int=None,
-                 layer3_size: int=None,
+                 layer1_size: int=128,
                  learning_rate: float=1e-3,
                  max_epochs: int=200,
+                 batch_size: int=64,
                  tol: float=1e-4,
                  n_consec_iter: int=10,
                  verbose: bool=True,
+                 random_state: int=None,
                 ) -> None:
         super().__init__()
+
+        # Set random seed for reproducibility
+        if random_state:
+            self.set_random_seed(random_state)
+        
         # Training parameters
         self.input_size = input_size
         self.layer1_size = layer1_size
-        if self.layer1_size == None:
-            self.layer1_size = int(2**np.ceil(np.log2(self.input_size)))
-        self.layer2_size = layer2_size
-        if self.layer2_size == None:
-            self.layer2_size = int(self.layer1_size/2)
-        self.layer3_size = layer3_size
-        if self.layer3_size == None:
-            self.layer3_size = max(16, int(self.layer2_size/2))
         self.learning_rate = learning_rate
         self.max_epochs = max_epochs
+        self.batch_size = batch_size
         self.tol = tol
         self.n_consec_iter = n_consec_iter
         self.verbose = verbose
 
         # Layers
         self.layer1 = nn.Linear(self.input_size, self.layer1_size)
-        self.layer2 = nn.Linear(self.layer1_size, self.layer2_size)
-        self.layer3 = nn.Linear(self.layer2_size, self.layer3_size)
-        self.layer_out = nn.Linear(self.layer3_size, 1)
+        self.layer_out = nn.Linear(self.layer1_size, 1)
         # Activation, Dropout, and Batch Normalisation
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.2)
+        self.dropout = nn.Dropout(p=0.3)
         self.batchnorm1 = nn.BatchNorm1d(self.layer1_size)
-        self.batchnorm2 = nn.BatchNorm1d(self.layer2_size)
-        self.batchnorm3 = nn.BatchNorm1d(self.layer3_size)
+
+    def set_random_seed(self, seed: int):
+        """
+        Sets the seed for reproducibility.
+        """
+        # Set the random seed for Python's random module, NumPy, and PyTorch
+        # random.seed(seed) # Uncomment if using module random
+        np.random.seed(seed)
+        torch.manual_seed(seed)  # CPU
+        torch.cuda.manual_seed(seed)  # GPU (if available)
+        torch.cuda.manual_seed_all(seed)  # All GPUs
+        # torch.backends.cudnn.deterministic = True  # Ensure deterministic behavior
+        # torch.backends.cudnn.benchmark = False  # Disable the auto-tuner for deterministic behavior
 
     def forward(self, x):
         # Layer 1
         x = self.relu(self.layer1(x))
-        x = self.batchnorm1(x)
-        x = self.dropout(x)
-        # Layer 2
-        x = self.relu(self.layer2(x))
-        x = self.batchnorm2(x)
-        x = self.dropout(x)
-        # Layer 3
-        x = self.relu(self.layer3(x))
-        x = self.batchnorm3(x)
+        # x = self.batchnorm1(x)
         x = self.dropout(x)
         # Output layer
         x = self.layer_out(x)
 
         return x
 
-    def fit(self, train_loader: DataLoader):
+    # Modified version of .fit() for pd.DataFrame
+    # def fit(self, train_loader: DataLoader):
+    def fit(self, X_train: pd.DataFrame, y_train: pd.DataFrame):
         # Send model to device
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.to(device)
-        
+
+        # Train DataLoader
+        train_data = TrainData(torch.tensor(X_train.values, dtype=torch.float32), 
+                   torch.tensor(y_train.values, dtype=torch.float32))
+        train_loader = DataLoader(dataset=train_data, batch_size=self.batch_size, shuffle=True)
+        if self.verbose: print(f'Train loader size: {len(train_loader)} batches')
+
         # Loss and Optimizer
         loss_function = nn.BCEWithLogitsLoss()
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
@@ -108,7 +117,7 @@ class BinaryMLP(nn.Module):
                 y_pred = torch.round(y_pred_prob)
 
                 # Compute loss
-                loss = loss_function(y_logit, y_batch.view(-1, 1))
+                loss = loss_function(y_logit, y_batch.unsqueeze(1))#.view(-1, 1))
                 epoch_loss += loss.item()
                 
                 # Backward pass and optimization
@@ -116,16 +125,16 @@ class BinaryMLP(nn.Module):
                 optimizer.step()
                 
                 # Compute metrics
-                recall.update(y_pred_prob.squeeze(), y_batch.int())
-                precision.update(y_pred_prob.squeeze(), y_batch.int())
-                f1.update(y_pred_prob.squeeze(), y_batch.int())
-                accuracy.update(y_pred_prob.squeeze(), y_batch.int())
+                recall.update(y_pred_prob.view(-1), y_batch.int())
+                precision.update(y_pred_prob.view(-1), y_batch.int())
+                f1.update(y_pred_prob.view(-1), y_batch.int())
+                accuracy.update(y_pred_prob.view(-1), y_batch.int())
 
             avg_loss = epoch_loss / len(train_loader)
             
             # Print progress first, last, and ten times
             if self.verbose:
-                if e % int(self.max_epochs/10) == 0 or e == 1 or e == self.max_epochs:
+                if (e % int(self.max_epochs/10) == 0 or e == self.max_epochs) or e == 1:
                     print(f'Epoch {e+0:03}: |',
                           f'Loss: {avg_loss:.5f} |',
                           f'Accuracy: {accuracy.compute().item():.3f} |',
@@ -136,24 +145,28 @@ class BinaryMLP(nn.Module):
             # Stop criterion
             if abs(avg_loss - avg_loss_old) <= self.tol:
                 count_iter += 1
-                print(f'Current Epoch: {e} |',
-                      f'Loss change: {abs(avg_loss - avg_loss_old):.1e} |',
-                      f'Consecutive stop iterations: {count_iter}')
+                if self.verbose:
+                    print(f'Current Epoch: {e} |',
+                          f'Loss change: {abs(avg_loss - avg_loss_old):.1e} |',
+                          f'Consecutive stop iterations: {count_iter}')
                 if count_iter == self.n_consec_iter:
-                    print('Early stop criterion reached.')
+                    if self.verbose: print('Early stop criterion reached.')
                     break
             else:
                 count_iter = 0
             avg_loss_old = avg_loss
-        print(f'Epoch {e+0:03}: |',
-              f'Loss: {avg_loss:.5f} |',
-              f'Accuracy: {accuracy.compute().item():.3f} |',
-              f'Recall: {recall.compute().item():.3f} |',
-              f'Precision: {precision.compute().item():.3f} |',
-              f'F1: {f1.compute().item():.3f}')
-        
-    def predict_proba(self, test_loader: DataLoader):
+
+    # Modified version of .predict_proba() for pd.DataFrame
+    # def predict_proba(self, test_loader: DataLoader):
+    def predict_proba(self, X_test: pd.DataFrame):
         device = next(self.parameters()).device  # Get the device from the model
+
+        # Test DataLoader
+        test_data = TestData(torch.tensor(X_test.values, dtype=torch.float32))
+        test_loader = DataLoader(dataset=test_data, batch_size=self.batch_size, shuffle=False)
+        # print(f'Test loader size: {len(test_loader)} batches')
+
+        # Make predictions
         y_pred_prob_list = []
         self.eval()
         with torch.no_grad():
@@ -165,13 +178,12 @@ class BinaryMLP(nn.Module):
                 y_logit = self(X_batch)
                 y_pred_prob_list.append(torch.sigmoid(y_logit))
         
-        return torch.cat(y_pred_prob_list, dim=0).squeeze().detach().numpy()
+        # Reshape for compatibility with sklearn
+        p = torch.cat(y_pred_prob_list, dim=0).squeeze().detach().numpy()
+        return np.concatenate([1-p.reshape(-1, 1), p.reshape(-1, 1)], axis=1)
         
-    def predict(self, test_loader: DataLoader):
-        y_pred_prob = self.predict_proba(test_loader)
+    def predict(self, X_test: pd.DataFrame):
+        y_pred_prob = self.predict_proba(X_test)[:, 1]
         return (y_pred_prob >= 0.5).astype(int)
-
-
-
 
 
--- fraudmrm/utils.py
+++ fraudmrm/utils.py
@@ -4,6 +4,33 @@ import pandas as pd
 from sklearn.metrics import recall_score, precision_score, f1_score, precision_recall_curve, roc_auc_score, roc_curve
 
 
+# MLP
+
+## train data
+class TrainData(Dataset):
+    
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+        
+    def __getitem__(self, index):
+        return self.X[index], self.y[index]
+        
+    def __len__ (self):
+        return len(self.X)
+
+## test data    
+class TestData(Dataset):
+    
+    def __init__(self, X):
+        self.X = X
+        
+    def __getitem__(self, index):
+        return self.X[index]
+        
+    def __len__ (self):
+        return len(self.X)
+
 def train_mlp(model, X_train: pd.DataFrame, y_train: pd.DataFrame, batch_size: int=64):
     # Train DataLoader
     train_data = TrainData(torch.tensor(X_train.values, dtype=torch.float32), 
@@ -14,7 +41,7 @@ def train_mlp(model, X_train: pd.DataFrame, y_train: pd.DataFrame, batch_size: i
     # Train model
     model.fit(train_loader)
 
-def evaluate_mlp(model, X_test, y_test, batch_size: int=64):
+def test_mlp(model, X_test, y_test, batch_size: int=64):
     # Test DataLoader
     test_data = TestData(torch.tensor(X_test.values, dtype=torch.float32))
     test_loader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=False)
@@ -24,18 +51,8 @@ def evaluate_mlp(model, X_test, y_test, batch_size: int=64):
     pred = model.predict(test_loader)
     # Prediction probabilities
     pred_prob = model.predict_proba(test_loader)
-    
-    # Metrics
-    metrics = {
-        'recall_score': recall_score(y_test, pred),
-        'precision_score': precision_score(y_test, pred),
-        'f1_score': f1_score(y_test, pred),
-        'precision_recall_curve': precision_recall_curve(y_test, pred_prob),
-        'roc_auc_score': roc_auc_score(y_test, pred_prob),
-        'roc_curve': roc_curve(y_test, pred_prob)
-    }
 
-    return metrics
+    return pred, pred_prob
 
 
 
